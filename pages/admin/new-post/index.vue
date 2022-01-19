a4fbf8fda73f922c0e22dddc6a4190cdfb94d2fc
oladetoungee <demilad1998@gmail.com>
2022-01-19 14:34:12 +0100

    I added firebase as backend and installed axios

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,18 +1,27 @@
 <template>
     <div class="admin-new-post-page">
         <section class="new-post-form">
-          <admin-post-forum/>  
+          <admin-post-forum @submit="onSubmitted"/>  
         </section>
     </div>
 </template>
 <script>
-
+import axios from 'axios'
 import AdminPostForum from '../../../components/Admin/AdminPostForum.vue';
 export default {
-    
+    layout: 'admin',
     components: {
         
         AdminPostForum,
+    },
+    methods: {
+        onSubmitted(postData) {
+            axios.post('https://gbemisblog-7a042-default-rtdb.firebaseio.com/posts.json', postData) 
+                .then(result => console.log(result))
+                .catch(e => console.log(e))
+            
+
+        }
     }
 }   
 </script>
